a60b6ddd1b5cbfec521e1539955474c7bf3b3a26
Pedro Gruvhagen - Demoskop <pedro@gruvhagen.com>
2025-08-15 13:08:38 +0200

    Initial Commit

---
+++ Notepad++/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notepad++
+//
+//  Created by Pedro Gruvhagen on 2025-08-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notepad++/Notepad__App.swift
@@ -0,0 +1,17 @@
+//
+//  Notepad__App.swift
+//  Notepad++
+//
+//  Created by Pedro Gruvhagen on 2025-08-15.
+//
+
+import SwiftUI
+
+@main
+struct Notepad__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
